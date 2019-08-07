0321907680a43f186d05dd94f0bf452bee51153e
HollyBang <mazafakand@gmail.com>
2019-08-07 21:43:57 +0300

    temporary deleted build folder cleaner

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const findProcessVariable = (key) => {
   let value = null;
@@ -90,7 +89,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebPackPlugin({
       template: './public/index.html',
       filename: './index.html'
